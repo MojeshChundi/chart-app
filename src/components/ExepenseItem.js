a9b519a37582d478bc1ea20cf6c386f6a159685e
MojeshChundi <mojeshchundi5432@gmail.com>
2022-10-26 16:08:19 +0530

    first react file

---
+++ src/components/ExepenseItem.js
@@ -0,0 +1,13 @@
+function ExepenseItem(){
+   return(
+    <div>
+    <h1> Exepense Item</h1>
+    <p>Food Rs 10</p>
+	<p>Petrol Rs 100</p>
+	<p>Movies Rs 200</p>
+    </div>
+   )
+
+}
+
+export default ExepenseItem;
