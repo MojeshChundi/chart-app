93ee00ea87fdb7c0920d3b8828876be4c93ce044
MojeshChundi <mojeshchundi5432@gmail.com>
2022-10-29 07:41:20 +0530

    react2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import ExepenseItem from './components/ExepenseItem';
+import ExepenseItem from "./components/ExepenseItem";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExepenseItem></ExepenseItem>
-
+      return(
+      {expenses.map((ele) => (
+        <ExepenseItem
+          date={ele.date}
+          title={ele.title}
+          amount={ele.amount}
+        ></ExepenseItem>
+      ))}
+      )
     </div>
   );
 }
--- src/components/ExepenseItem.js
+++ src/components/ExepenseItem.js
@@ -1,13 +1,15 @@
-function ExepenseItem(){
-   return(
-    <div>
-    <h1> Exepense Item</h1>
-    <p>Food Rs 10</p>
-	<p>Petrol Rs 100</p>
-	<p>Movies Rs 200</p>
-    </div>
-   )
+import "./ExepenseItem.css";
 
+function ExepenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExepenseItem;
