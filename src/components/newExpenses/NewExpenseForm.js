ec41a7f6708fcf021a313b36093e2ba2c386f105
MojeshChundi <mojeshchundi5432@gmail.com>
2022-11-06 22:47:12 +0530

    multiple UseState

--- src/components/newExpenses/NewExpenseForm.js
+++ src/components/newExpenses/NewExpenseForm.js
@@ -1,36 +1,58 @@
 //import react from "react";
-import { useRef } from "react";
+import { useState } from "react";
 import "./NewExpensForm.css";
 const NewExpenseForm = () => {
-  const useref = useRef(null);
-  const seref = useRef(null);
-  const eref = useRef(null);
-  const clickHandle = (e) => {
-    e.preventDefault();
+  const [titleEntered, setTitle] = useState("");
+  const [amountEntered, setAmount] = useState("");
+  const [dateEntered, setDate] = useState("");
 
-    console.log(useref.current.value);
-    console.log(seref.current.value);
-    console.log(eref.current.value);
+  const amountHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const titleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const dateHandler = (event) => {
+    setDate(event.target.value);
+  };
+  const clickHandler = (event) => {
+    event.preventDefault();
+    const inputData = {
+      title: titleEntered,
+      amount: amountEntered,
+      date: dateEntered,
+    };
+    console.log(inputData);
   };
 
   return (
-    <form>
+    <form onSubmit={clickHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
           <label>title</label>
-          <input type="text" ref={seref} />
+          <input type="text" onChange={titleHandler} />
         </div>
         <div className="new-expense__controls">
           <label>amount</label>
-          <input type="number" step="0.01" min="0.01" ref={eref} />
+          <input
+            type="number"
+            step="0.01"
+            min="0.01"
+            onChange={amountHandler}
+          />
         </div>
         <div className="new-expense__controls">
           <label>Date</label>
-          <input type="Date" min="2019-01-10" max="2022-12-25" ref={useref} />
+          <input
+            type="Date"
+            min="2019-01-10"
+            max="2022-12-25"
+            onChange={dateHandler}
+          />
         </div>
       </div>
       <div className="new-expense__action">
-        <button onClick={clickHandle}>Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
