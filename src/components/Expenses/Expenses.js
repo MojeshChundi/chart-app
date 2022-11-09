9d36eadf210d2b2ecaf6928fbc91c0a8435a8d40
MojeshChundi <mojeshchundi5432@gmail.com>
2022-11-09 12:01:12 +0530

    event listner

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import ExepenseItem from "./ExepenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 import "./Expenses.css";
 function Expenses() {
   const expense = [
@@ -25,8 +26,17 @@ function Expenses() {
     },
   ];
 
+  const [filterYear, setSelectedYear] = useState("2020");
+
+  const yearHandler = (selectedYear) => {
+    console.log("Expense");
+    setSelectedYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filterYear} SelectYear={yearHandler} />
+
       {expense.map((ele) => (
         <ExepenseItem date={ele.date} title={ele.title} amount={ele.amount} />
       ))}
